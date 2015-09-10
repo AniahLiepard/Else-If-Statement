57cdc9710429a79e4ed3eaf90c58eb00dd5da68c
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 10:40:16 -0400

    Else If Statement Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17984

---
+++ ProjectSixteen.java
@@ -0,0 +1,13 @@
+public class ProjectSixteen {
+	public static void main(String args[]){
+		int taco = 21;
+		
+		if (taco <= 5){
+			System.out.println("INSUFFICIENT TACO AMOUNT.");
+		}else if (taco >= 80){
+			System.out.println("You are a taco god.");
+			}else{
+			System.out.println("You are very rich in tacos.");
+		 }
+	   }
+	}
